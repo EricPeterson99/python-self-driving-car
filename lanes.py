2399972ca0d9efc027ad4a45690f11a97a797b61
Eric Peterson <Phxeric93106@gmail.com>
2019-11-17 15:13:25 -0700

    Optomized code to find the average line computed for the left and right side and used this to create one continuous line

--- lanes.py
+++ lanes.py
@@ -27,21 +27,52 @@ def region_of_interest(image):
 def display_lines(image, lines):
     line_image = np.zeros_like(image)
     if lines is not None:
-        for line in lines:
-            x1, y1, x2, y2 = line.reshape(4)
-            cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
+        for x1, y1, x2, y2 in lines:
+            cv2.line(line_image, (x1, y1), (x2, y2),
+                     (255, 0, 0), 10)  # draw lines
     return line_image
+
+
+def average_slope_intercept(image, lines):
+    left_fit = []
+    right_fit = []
+    for line in lines:  # For each line find the slope and intercept and add data to correct side
+        x1, y1, x2, y2 = line.reshape(4)
+        parameters = np.polyfit((x1, x2), (y1, y2), 1)
+        slope = parameters[0]
+        intercept = parameters[1]
+        if slope < 0:
+            left_fit.append((slope, intercept))
+        else:
+            right_fit.append((slope, intercept))
+
+    left_fit_average = np.average(left_fit, axis=0)  # compute average
+    right_fit_average = np.average(right_fit, axis=0)
+    left_line = make_coordinates(image, left_fit_average)  # get cordinates
+    right_line = make_coordinates(image, right_fit_average)
+    return np.array([left_line, right_line])
+
+
+def make_coordinates(image, line_parameters):
+    slope, intercept = line_parameters
+    y1 = image.shape[0]  # bottom of image
+    y2 = int(y1 * (3/5))  # 3/5 up the image
+    x1 = int((y1-intercept)/slope)      # x = (y-b)/m
+    x2 = int((y2-intercept)/slope)
+    return np.array([x1, y1, x2, y2])
 
 
 image = cv2.imread('Image/test_image.jpg')
 lane_image = np.copy(image)
 
-canny = canny(lane_image)
-cropped_image = region_of_interest(canny)
+canny_image = canny(lane_image)
+cropped_image = region_of_interest(canny_image)
 
 lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100,
                         np.array([]), minLineLength=40, maxLineGap=5)
-line_image = display_lines(lane_image, lines)
+averaged_lines = average_slope_intercept(lane_image, lines)
+
+line_image = display_lines(lane_image, averaged_lines)
 
 combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
 cv2.imshow('result', combo_image)
